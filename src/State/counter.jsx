0200b7f091ef5a1fdd040a064dbf90846e38b3cd
Sunilchow19 <you@example.com>
2024-10-23 09:34:41 +0530

    first commit

---
+++ src/State/counter.jsx
@@ -0,0 +1,70 @@
+import { Component } from "react";
+
+
+
+
+class Counter extends Component{
+
+
+    //With constructor defiining State
+    constructor(){
+        super()
+        this.plus=this.plus.bind(this)
+        this.minus=this.minus.bind(this)
+        this.state={
+            a:0
+        }
+    }
+
+  //Without constructor defining State
+//   state={
+//     a:10
+//   }
+
+    plus(){
+        this.setState({
+            a:this.state.a+1
+        })
+    }
+
+    minus(){
+        this.setState({
+            a:this.state.a-1
+        })
+    }
+
+    render(){
+
+        let counterStyle = {};
+    if (this.state.a > 5) {
+      counterStyle = { color: "red" };
+    } else if (this.state.a < 0) {
+      counterStyle = { color: "blue" };
+    } else {
+      counterStyle = { color: "green" };
+    }
+
+
+        return (
+            <>
+
+
+            
+            <h1>Counter</h1>
+
+            <button onClick={this.minus} >-</button>
+            <span style={counterStyle}>{this.state.a}</span>
+            
+            <button onClick={this.plus}>+</button>
+        
+
+            
+
+            
+            </>
+        )
+    }
+}
+
+
+export default Counter
